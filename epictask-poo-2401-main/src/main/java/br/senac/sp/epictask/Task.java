f73a5b40b37194029339b77206e5790ced1cea44
Guilherme Soares <126208556+Guisoaresz13@users.noreply.github.com>
2024-05-17 22:03:47 -0300

    Add files via upload

---
+++ epictask-poo-2401-main/src/main/java/br/senac/sp/epictask/Task.java
@@ -0,0 +1,35 @@
+package br.senac.sp.epictask;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+public class Task {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+     //@Pattern(regexp = "DEBITO|CREDITO|PIX")
+     //private String formaDePagamento;
+
+    // @PastOrPresent
+    // private LocalDate dataPagamento;
+
+    
+    private String description;
+
+    @Min(0)
+    private Integer score;
+
+    private Integer status;
+
+}
---
+++ epictask-poo-2401-main/src/main/java/br/senac/sp/epictask/TaskController.java
@@ -0,0 +1,64 @@
+package br.senac.sp.epictask;
+
+import java.util.List;
+
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("task")
+public class TaskController {
+
+    @Autowired
+    TaskRepository repository;
+
+    @Autowired
+    OpenAiChatClient gpt;
+
+    @GetMapping
+    public String index(Model model, @AuthenticationPrincipal OAuth2User user) {
+        List<Task> tasks = repository.findAll();
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("user", user);
+        return "task/index";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String destroy(@PathVariable Long id) {
+        repository.deleteById(id);
+        return "redirect:/task";
+    }
+
+    @GetMapping("new")
+    public String form(Task task) {
+        return "task/new";
+    }
+
+    @PostMapping
+    public String create(@Valid Task task, BindingResult result) {
+        if (result.hasErrors())
+            return "task/new";
+
+        if (task.getDescription() == null || task.getDescription().isBlank()) {
+            task.setDescription(gpt.call("Crie a descrição de uma tarefa com o titulo, . A descrição não pode ter mais que 200 caracteres" + task.getTitle()));
+
+        }
+
+        repository.save(task);
+        return "redirect:/task";
+    }
+
+}
